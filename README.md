7d0ebe05597c4c6594b4a42fed67decbffb6ae1e
yhirose <yuji.hirose.bug@gmail.com>
2021-10-16 18:17:53 -0400

    Simplified interface

--- README.md
+++ README.md
@@ -8,10 +8,8 @@ std::string key;
 jsonlib::parse(json, {
   .Key = [&](auto str, auto length, auto copy) {
     key.assign(str, length);
-    return true;
   },
   .String = [&](auto str, auto length, auto copy) {
     std::cout << key << ": " << std::string(str, length) << std::endl;
-    return true;
   }});
 ```
--- jsonlib.h
+++ jsonlib.h
@@ -17,19 +17,19 @@ namespace jsonlib {
 using namespace rapidjson;
 
 struct Handlers {
-  std::function<bool()> Null;
-  std::function<bool(bool b)> Bool;
-  std::function<bool(int i)> Int;
-  std::function<bool(unsigned u)> Uint;
-  std::function<bool(int64_t i)> Int64;
-  std::function<bool(uint64_t i)> Uint64;
-  std::function<bool(double i)> Double;
-  std::function<bool(const char* str, SizeType length, bool copy)> String;
-  std::function<bool()> StartObject;
-  std::function<bool(const char* str, SizeType length, bool copy)> Key;
-  std::function<bool(SizeType memberCount)> EndObject;
-  std::function<bool()> StartArray;
-  std::function<bool(SizeType elementCount)> EndArray;
+  std::function<void()> Null;
+  std::function<void(bool b)> Bool;
+  std::function<void(int i)> Int;
+  std::function<void(unsigned u)> Uint;
+  std::function<void(int64_t i)> Int64;
+  std::function<void(uint64_t i)> Uint64;
+  std::function<void(double i)> Double;
+  std::function<void(const char* str, SizeType length, bool copy)> String;
+  std::function<void()> StartObject;
+  std::function<void(const char* str, SizeType length, bool copy)> Key;
+  std::function<void(SizeType memberCount)> EndObject;
+  std::function<void()> StartArray;
+  std::function<void(SizeType elementCount)> EndArray;
 };
 
 inline void parse(const char* json, const Handlers& functions) {
@@ -38,91 +38,91 @@ inline void parse(const char* json, const Handlers& functions) {
 
     bool Null() {
       if (functions_.Null) {
-        return functions_.Null();
+        functions_.Null();
       }
       return true;
     }
 
     bool Bool(bool b) {
       if (functions_.Bool) {
-        return functions_.Bool(b);
+        functions_.Bool(b);
       }
       return true;
     }
 
     bool Int(int i) {
       if (functions_.Int) {
-        return functions_.Int(i);
+        functions_.Int(i);
       }
       return true;
     }
 
     bool Uint(unsigned u) {
       if (functions_.Uint) {
-        return functions_.Uint(u);
+        functions_.Uint(u);
       }
       return true;
     }
 
     bool Int64(int i) {
       if (functions_.Int64) {
-        return functions_.Int64(i);
+        functions_.Int64(i);
       }
       return true;
     }
 
     bool Uint64(unsigned u) {
       if (functions_.Uint64) {
-        return functions_.Uint64(u);
+        functions_.Uint64(u);
       }
       return true;
     }
 
     bool Double(double d) {
       if (functions_.Double) {
-        return functions_.Double(d);
+        functions_.Double(d);
       }
       return true;
     }
 
     bool String(const char* str, SizeType length, bool copy) {
       if (functions_.String) {
-        return functions_.String(str, length, copy);
+        functions_.String(str, length, copy);
       }
       return true;
     }
 
     bool StartObject() {
       if (functions_.StartObject) {
-        return functions_.StartObject();
+        functions_.StartObject();
       }
       return true;
     }
 
     bool Key(const char* str, SizeType length, bool copy) {
       if (functions_.Key) {
-        return functions_.Key(str, length, copy);
+        functions_.Key(str, length, copy);
       }
       return true;
     }
 
     bool EndObject(SizeType memberCount) {
       if (functions_.EndObject) {
-        return functions_.EndObject(memberCount);
+        functions_.EndObject(memberCount);
       }
       return true;
     }
 
     bool StartArray() {
       if (functions_.StartArray) {
-        return functions_.StartArray();
+        functions_.StartArray();
       }
       return true;
     }
 
     bool EndArray(SizeType elementCount) {
       if (functions_.EndArray) {
-        return functions_.EndArray(elementCount);
+        functions_.EndArray(elementCount);
       }
       return true;
     }
